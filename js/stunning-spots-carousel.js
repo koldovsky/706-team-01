e57c334ab4889efa25e7b9e36f990158a22b36c1
AleksandraVol <volkovaa3105@gmail.com>
2022-08-09 10:46:45 +0000

    fixed bugs added favicon

--- js/stunning-spots-carousel.js
+++ js/stunning-spots-carousel.js
@@ -1,10 +1,10 @@
 (function () {
     const slides = [
         `<div class="stunning-spots-slide">
-        <img src="img/stunning-spots-norway-landscapes/norway-landscapes-1.jpeg" class="image-slide" alt="norway-landscapes"
+        <img onclick="img/stunning-spots-norway-landscapes/norway-landscapes-1.jpeg" src="img/stunning-spots-norway-landscapes/norway-landscapes-1.jpeg" class="image-slide" alt="norway-landscapes"
         </div>`,
             `<div class="stunning-spots-slide">
-        <img src="img/stunning-spots-norway-landscapes/norway-landscapes-2.jpeg" class="image-slide" alt="norway-landscapes"
+        <img onclick="img/stunning-spots-norway-landscapes/norway-landscapes-2.jpeg" src="img/stunning-spots-norway-landscapes/norway-landscapes-2.jpeg" class="image-slide" alt="norway-landscapes"
         </div>`,
             `<div class="stunning-spots-slide">
         <img src="img/stunning-spots-norway-landscapes/norway-landscapes-3.jpeg" class="image-slide" alt="norway-landscapes"
@@ -28,9 +28,9 @@
         if (window.innerWidth > 768) {
             const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
             slideContainer.innerHTML += slides[secondSlideIdx];
-            if (window.innerWidth > 992) {
-                const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
-                slideContainer.innerHTML += slides[thirdSlideIdx];
+        if (window.innerWidth > 992) {
+            const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+            slideContainer.innerHTML += slides[thirdSlideIdx];
             }
         }
     }
@@ -44,33 +44,13 @@
         renderCarousel();
     }
 
-    // setInterval(next, 3000);
+    setInterval(next, 5000);
 
     const prevButton = document.querySelector('.stunning-spots-carousel__btn-prev');
     prevButton.addEventListener('click', prev);
 
     const nextButton = document.querySelector('.stunning-spots-carousel__btn-next');
     nextButton.addEventListener('click', next);
-    
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName(".stunning-spots-slide");
-        var dots = document.getElementsByClassName(".slider-dots");
-        if (n > slides.length) {
-          slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-    }
 
     renderCarousel();
 
